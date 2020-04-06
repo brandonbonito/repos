437fd317ecf8de2ab489d5d4aa35a77724e4dd3e
brandonbonito <brandonbonito@users.noreply.github.com>
2020-04-06 16:23:39 -0400

    Initial commit

---
+++ NumberGuessingGame/NumberGuessingGame/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NumberGuessingGame.Models;
+using System;
+using System.Diagnostics;
+
+namespace NumberGuessingGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ThreadStatic] private static Random _random;
+        private static int _answer;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return LoadPage();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public JsonResult GetAnswer(int guess)
+        {
+            if (_answer > guess)
+                return Json("Your guess is too low.");
+            if (_answer < guess)
+                return Json("Your guess is too high.");
+            if (_answer == guess)
+                return Json("Congratulations!");
+
+            return Json(_answer);
+        }
+
+        [HttpPost]
+        public ActionResult PlayAgain()
+        {
+            return LoadPage();
+        }
+
+        public ActionResult LoadPage()
+        {
+            ModelState.Clear();
+            _random = new Random();
+            _answer = _random.Next(1, 100);
+            return View(new RandomNumberModel());
+        }
+    }
+}
---
+++ NumberGuessingGame/NumberGuessingGame/Models/RandomNumberModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace NumberGuessingGame.Models
+{
+    public class RandomNumberModel
+    {
+        [Required]
+        [FromRoute]
+        [Display(Name = "Please enter your guess (1-100): ")]
+        public int Guess { get; set; }
+
+        public int Answer { get; set; }
+
+        [Display(Name = "Would you like to play again?")]
+        public bool PlayAgain { get; set; }
+    }
+}
